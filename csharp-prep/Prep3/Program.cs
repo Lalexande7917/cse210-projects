a25351d7a01100a800c1cd59dd9266a2f53c1c60
Corben <Lalexande7917@gmail.com>
2024-05-01 22:10:40 -0600

    Number Guessing While loop

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,30 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Console.Write("What is the magic number? ");
+        string magicNumber = Console.ReadLine();
+        int num = int.Parse(magicNumber);
+
+        int guess;
+        do 
+        {
+        Console.Write("What is your guess? ");
+        string g = Console.ReadLine();
+        guess = int.Parse(g);
+
+
+            if (guess > num)
+            {
+                Console.WriteLine("The number is Lower");
+            }
+            else if (guess < num)
+            {
+                Console.WriteLine("The number is Higher");
+            }
+            else
+            {
+                Console.WriteLine("You have guessed the correct number! ");
+            }
+        } while (guess != num);
     }
 }

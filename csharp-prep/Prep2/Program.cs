a1de585b92ab2e8d48207f3f9f7bf289eba121bd
Corben <Lalexande7917@gmail.com>
2024-05-01 21:44:23 -0600

    Grade Letter code for prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -1,9 +1,33 @@
 using System;
+using System.Globalization;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage? ");
+        string gradeLetter = Console.ReadLine();
+        int grade = int.Parse(gradeLetter);
+        
+        if (grade >= 90)
+        {
+            Console.WriteLine("Your grade is an A");
+        }
+        else if (grade >= 80)
+        {
+            Console.WriteLine("Your grade is a B");
+        }
+        else if (grade >= 70)
+        {
+            Console.WriteLine("Your grade is a C");
+        }
+        else if (grade >= 60)
+        {
+            Console.WriteLine("Your grade is a D");
+        }
+        else
+        {
+            Console.WriteLine("Your Grade is an F");
+        }
     }
 }

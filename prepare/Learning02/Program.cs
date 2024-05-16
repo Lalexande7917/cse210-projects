b5f0cfea2051b5f196221dd601363d2c9b60dff7
Corben <Lalexande7917@gmail.com>
2024-05-16 14:47:03 -0600

    added job and resume classes

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -5,5 +5,24 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Software Engineer";
+        job1._company = "Microsoft";
+        job1._startYear = 2023;
+        job1._endYear = 2024;
+        job1.display();
+
+        Job job2 = new Job();
+        job2._jobTitle = "Manager";
+        job2. _company = "Apple";
+        job2._startYear = 2013;
+        job2._endYear = 2017;
+        job2.display();
+
+        Resume resume1 = new Resume();
+        resume1._name = "Corben";
+        resume1._jobs.Add(job1);
+        resume1._jobs.Add(job2);
+        resume1.Display();
     }
 }

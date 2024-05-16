4c2fbe0222328a81df9395e9b12d33a2ebf0cf76
Corben <Lalexande7917@gmail.com>
2024-05-16 13:36:37 -0600

    sum of numbers Program

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -4,6 +4,26 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        int numberToAdd;
+        int sum = 0;
+        do
+        {
+            Console.Write("Enter a number: ");
+            string num = Console.ReadLine();
+            numberToAdd = int.Parse(num);
+            if (numberToAdd != 0 )
+            {
+                numbers.Add(numberToAdd);
+                sum += numberToAdd;
+            }
+        }   while (numberToAdd != 0);
+        Console.WriteLine("You entered the following numbers: ");
+        foreach (int number in numbers)
+        {
+            Console.WriteLine(number);
+        }
+        Console.WriteLine($"The sum of all these numbers is: {sum}");
     }
 }
